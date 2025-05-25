694b23c209b7704ce94349ecc929df4be9248ccb
Julius Ware <jcware8@gmail.com>
2025-05-25 16:29:44 -0400

    first commit, app mostly running.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // 1) Import the React and ReactDOM libraries and App component
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App";
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -9,5 +12,11 @@ const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Show the component on the screen
-root.render(<App />)
+// 5) Show the component on the screen
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
